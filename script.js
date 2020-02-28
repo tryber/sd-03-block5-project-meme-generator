984006616a9d17359d5aaa5bf115d49aeac161dc
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-28 15:19:30 -0300

    passando moldura para js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  const changeImg = () => {
+    console.log(document.getElementById("image-input").value);
+    document.getElementById("meme-image").src = document.getElementById("image-input").value
+    document.getElementById("meme-image").style.borderColor = "black";
+    document.getElementById("meme-image").style.borderWidth = "1px";
+    document.getElementById("meme-image").style.borderStyle = "solid";
+  };
+  document.getElementById("image-input").addEventListener("change", changeImg)
+};
