559abc950fe3cebf9caf7c42c2e764884cfba917
Jafet Henrique <jafetguerra6@gmail.com>
2020-02-28 16:58:42 -0300

    Adicionando o texto e a borda

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 window.onload = function() {
   let inputText = document.getElementById("text-input");
   let inputImage = document.querySelector("#image-input");
+  let paragraph = document.getElementById("meme-text");
   
   function UploadingImage(event) {
     let img = document.getElementById("meme-image")
     img.src = URL.createObjectURL(event.target.files[0]);;
   }
-
   inputImage.addEventListener("change", UploadingImage);
+
+  function writingText() {
+    paragraph.innerHTML = inputText.value
+  }
+  
+  inputText.addEventListener("keyup", writingText);
+
 }
