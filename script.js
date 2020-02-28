65f5825ec65e74a8541e8f9ba8396511d8b57dd4
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-28 16:39:08 -0300

    editando css/2

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let textInput = document.getElementById("text-input");
 let imageInput = document.getElementById("input-image");
 let memeImage = document.getElementById("meme-image");
+let memeText = document.getElementById("meme-text")
 
 function addMeme() {
     memeImage.src = URL.createObjectURL(imageInput.files[0]);
 }
 
 imageInput.addEventListener("change", addMeme);
+
+function addText() {
+    memeText.innerHTML = textInput.value
+}
+
+textInput.addEventListener("keyup", addText);
