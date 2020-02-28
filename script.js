d6bd9f60c79c85a761e3904db97986227325c5eb
wscatao <wscatao@gmail.com>
2020-02-28 17:42:27 -0300

    Adicionado o upload de img e criado o elemento div para inserir o texto

---
+++ script.js
@@ -0,0 +1,20 @@
+let caixaTexto = document.querySelector("#text-input")
+caixaTexto.addEventListener("input", criarTexto)
+
+let texto = document.querySelector(".texto")
+
+
+
+
+let x = document.getElementById("uploaderImage")
+x.addEventListener("change", carregarImagem)
+
+function carregarImagem() {
+    let arquivo = this.files[0]
+    let img = document.getElementsByTagName("img")
+    img[0].src = URL.createObjectURL(arquivo)
+}
+
+function criarTexto() {
+    texto.innerHTML = caixaTexto.value
+}
