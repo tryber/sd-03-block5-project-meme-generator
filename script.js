dbfd70d9a11ed7b84a8a774d7d3a8781c929f67f
Gabryel Ryba <gabryel.ryba@gmail.com>
2020-02-28 16:36:42 -0300

    iniciando

---
+++ script.js
@@ -0,0 +1,15 @@
+let textBox = document.getElementById('text-input');
+
+let textOutput = document.getElementById('meme-text');
+
+let image = document.getElementById('meme-image');
+
+textBox.addEventListener('keyup', changeText);
+
+function changeText () {
+textOutput.innerHTML = textBox.value;
+}
+
+function loadImage() {
+    image.src = URL.createObjectURL(event.target.files[0]);
+}
