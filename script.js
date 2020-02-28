c67f551e1bfa7cf66390a9401529ad542f974b27
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-28 15:54:18 -0300

    editando css

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let lugarMeme = document.getElementById("lugarMeme");
 
 function addMeme() {
     lugarMeme.src = URL.createObjectURL(memeImage.files[0]);
-    lugarMeme.style.width = 400 + "px";
-    lugarMeme.style.height = 400 + "px";
 }
 
 memeImage.addEventListener("change", addMeme);
