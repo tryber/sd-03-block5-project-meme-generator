eecd32e9dd0b871f06d758f8ca2cc32f3e861f97
Felipe Gomes <contato.felipeluna@gmail.com>
2020-02-28 14:59:37 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

---
+++ script.js
@@ -0,0 +1,11 @@
+let mostraFoto = function(event) {
+    let imagem = document.getElementById('meme-image');
+    imagem.src = URL.createObjectURL(event.target.files[0]);
+  };
+
+let texto = document.getElementById("text-input");
+texto.addEventListener('input', exibir);
+
+function exibir(){
+  document.getElementById("frase").innerHTML = texto.value;
+}
