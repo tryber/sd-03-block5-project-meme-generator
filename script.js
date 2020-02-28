a9713981a222e7217b010f60ac9327c31f170837
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-28 16:34:30 -0300

    adicionando borda na div da imagem

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = () => {
   document.getElementById("image-input").addEventListener("change", (event) => {
     document.getElementById('meme-image').src = URL.createObjectURL(event.target.files[0]);
-    document.getElementById("meme-image").style.borderColor = "black";
-    document.getElementById("meme-image").style.borderWidth = "1px";
-    document.getElementById("meme-image").style.borderStyle = "solid";
-    document.getElementById("meme-image").style.backgroundColor = "white";
+    // document.getElementById("meme-image").style.borderColor = "black";
+    // document.getElementById("meme-image").style.borderWidth = "1px";
+    // document.getElementById("meme-image").style.borderStyle = "solid";
+    // document.getElementById("meme-image").style.backgroundColor = "white";
   } )
 
   document.getElementById("text-input").addEventListener("change", (event) => {
