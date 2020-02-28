7bb0cba768db71e3930bc6a72d6ccac84b398eb1
luizfernando <luizfernandotic@gmail.com>
2020-02-28 16:22:22 -0300

    atendendo os requisitos principais

---
+++ script.js
@@ -0,0 +1,16 @@
+let loadFile = function(event) {
+	let image = document.getElementById('meme-image');
+	image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+let textoDoMeme = document.getElementById("text-input")
+
+
+function textonaimagem() {
+   //document.getElementById('foto').innerHTML = textoDoMeme.value; 
+   document.getElementById('meme-text').innerHTML = textoDoMeme.value;
+}
+
+
+
+textoDoMeme.addEventListener("keyup", textonaimagem);
