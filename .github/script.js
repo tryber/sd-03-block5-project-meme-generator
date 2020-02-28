943d020ffdd849ab06cf93cc8cfa3ff3f6a9a0ef
DiegoRafaelAF <draf0314@gmail.com>
2020-02-28 18:16:00 -0300

    verificando Codigo

---
+++ .github/script.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  let addImage = document.getElementById("input-image");
+  let addText = document.getElementById("text-input");
+  let memeText = document.getElementById("meme-text");
+
+  function criandoUrl(event) {
+    let image = document.getElementById("meme-image");
+    image.src = URL.createObjectURL(event.target.files[0]);
+  };
+  addImage.addEventListener("change", criandoUrl);
+
+  function inputText(input) {
+    memeText.textContent = input.target.value;
+  }
+  addText.addEventListener("beforeinput", inputText);
+}
