5d5ea1aea23f1fa40b3153f69257f4b51ac91f50
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-28 18:03:46 -0300

    trocando id do input de imagem

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  document.getElementById("image-input").addEventListener("change", (event) => {
+  document.getElementById("meme-insert").addEventListener("change", (event) => {
     document.getElementById('meme-image').src = URL.createObjectURL(event.target.files[0]);
     // document.getElementById("meme-image").style.borderColor = "black";
     // document.getElementById("meme-image").style.borderWidth = "1px";
