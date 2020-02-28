37a913e961876d487a371b7b8334f179ec080e61
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-28 18:49:14 -0300

    add buttons

--- script.js
+++ script.js
@@ -4,14 +4,46 @@ let memeInsert = document.getElementById("meme-insert");
 let memeImage = document.getElementById("meme-image");
 let memeText = document.getElementById("meme-text");
 
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+
 function addMeme() {
     memeImage.src = URL.createObjectURL(memeInsert.files[0]);
 }
-
-memeInsert.addEventListener("change", addMeme);
 
 function addText() {
     memeText.innerHTML = textInput.value;
 }
 
+function addBorder(indicador) {
+    if (indicador == 1) {
+        memeImage.style.borderColor = "red";
+        memeImage.style.borderWidth = 3 + "px";
+        memeImage.style.borderStyle = "dashed";
+    } else if (indicador == 2) {
+        memeImage.style.borderColor = "blue";
+        memeImage.style.borderWidth = 5 + "px";
+        memeImage.style.borderStyle = "double";
+    } else if (indicador == 3) {
+        memeImage.style.borderColor = "green";
+        memeImage.style.borderWidth = 6 + "px";
+        memeImage.style.borderStyle = "groove";
+    }
+}
+
+memeInsert.addEventListener("change", addMeme);
+
 textInput.addEventListener("keyup", addText);
+
+button1.addEventListener("click", function () {
+    addBorder(1);
+});
+
+button2.addEventListener("click", function () {
+    addBorder(2);
+});
+
+button3.addEventListener("click", function () {
+    addBorder(3);
+});
