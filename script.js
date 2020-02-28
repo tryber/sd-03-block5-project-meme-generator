65e52670db8cfc2fc5a2648a0a7112ef08a28adc
rogerfaustino <roger_faustino@hotmail.com>
2020-02-28 17:06:46 -0300

    Starting the meme-generator project

---
+++ script.js
@@ -0,0 +1,42 @@
+
+window.onload = function() {
+	previewFiles();
+	textInput();
+}
+
+function previewFiles() {
+
+  let preview = document.querySelector('#preview');
+  let files   = document.getElementById('meme-image').files;
+// let files   = document.querySelector('input[type=file]').files;
+
+  function readAndPreview(file) {
+    let reader = new FileReader();
+    
+    reader.addEventListener("load", function () {
+    let image = new Image();
+		image.height = 512;
+		image.width = 512;
+    image.title = file.name;
+    image.src = this.result;
+    preview.appendChild( image );
+    }, false);
+
+      reader.readAsDataURL(file);
+  }
+
+  if (files) {
+    [].forEach.call(files, readAndPreview);
+  }
+}
+
+function textInput(topText, bottomText) {
+
+  let topTextInput = document.querySelector('#text-input');
+  let bottomTextInput = document.querySelector('#text-input');
+
+  topTextInput.addEventListener("compositionupdate", function() {
+    console.log(topTextInput.value)
+
+  })
+}
