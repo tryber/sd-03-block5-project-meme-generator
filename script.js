17d35c2e9f9232c7a31882d5c3cbb755c672ceaf
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-28 18:22:19 -0300

    editando css

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ let memeInsert = document.getElementById("meme-insert");
 let memeImage = document.getElementById("meme-image");
 let memeText = document.getElementById("meme-text");
 
-
-
 function addMeme() {
     memeImage.src = URL.createObjectURL(memeInsert.files[0]);
-    memeImage.style.height = 400 + "px";
-    memeImage.style.width = 400 + "px";
 }
 
 memeInsert.addEventListener("change", addMeme);
