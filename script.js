906f3a92f900f63bebae2a82c2e2c387c8f6c68e
alcantara2509 <alcantara2509@gmail.com>
2020-02-28 18:17:47 -0300

    incluindo o id meme_insert

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let text_input = document.querySelector('#text-input');
 let meme_text = document.querySelector('#meme-text');
 let meme_image = document.querySelector('#meme-image');
-let img_input = document.querySelector('#img-input');
+let meme_insert = document.querySelector('#meme-insert');
 
 text_input.addEventListener('input', function changeText(){
   meme_text.innerHTML = text_input.value
 })
 
-img_input.addEventListener('change', function() {
+meme_insert.addEventListener('change', function() {
   document.getElementById('meme-image').src = window.URL.createObjectURL(this.files[0]);
 })
