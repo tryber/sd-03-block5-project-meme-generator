a78872d5685ec214998bdaf63ba4350579d9db2c
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-02-28 15:02:06 -0300

    Renomeando

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const img_input = document.getElementById('input_img');
-const memetextdown = document.getElementById('text-input');
+const img_input = document.getElementById('input-img');
+const textinput = document.getElementById('text-input');
 const memetextup = document.getElementById('textup');
 
 const imgbox = document.getElementById('imgbox');
@@ -8,8 +8,8 @@ img_input.addEventListener ('change', function(){
     document.getElementById('meme-image').src = window.URL.createObjectURL(this.files[0]);
 });
 
-memetextdown.addEventListener('keyup', function(){
-    document.getElementById('meme-text').innerText = memetextdown.value;
+textinput.addEventListener('keyup', function(){
+    document.getElementById('meme-text').innerText = textinput.value;
 });
 
 memetextup.addEventListener('keyup', function(){
