058b11998b06b9d3f48f329b23f6255ae760adde
Jafet Henrique <jafetguerra6@gmail.com>
2020-02-28 18:02:27 -0300

    Adicionando botões e word-break

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ window.onload = function() {
   let inputImage = document.querySelector("#image-input");
   let paragraph = document.getElementById("meme-text");
   
-  function UploadingImage(event) {
+  function UploadingImage() {
     let img = document.getElementById("meme-image")
-    img.src = URL.createObjectURL(event.target.files[0]);
+    img.src = URL.createObjectURL(inputImage.files[0]);
   }
   inputImage.addEventListener("change", UploadingImage);
 
@@ -14,5 +14,29 @@ window.onload = function() {
   }
   
   inputText.addEventListener("keyup", writingText);
+
+  let button1 = document.getElementById("button1");
+  let button2 = document.getElementById("button2");
+  let button3 = document.getElementById("button3");
+  let containerImage = document.getElementById("image-container");
+
+  function border1() {
+    containerImage.style.border = "3px dashed red";
+  }
+
+  button1.addEventListener("click", border1);
+
+  function border2() {
+    containerImage.style.border = "5px double blue";
+  }
+
+  button2.addEventListener("click", border2);
+
+  function border3() {
+    containerImage.style.border = "6px groove green";
+  }
+
+  button3.addEventListener("click", border3)
+
 
 }
