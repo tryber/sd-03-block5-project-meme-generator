3f885f6dbfc7a742b2302bc603e062112a913147
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-28 18:47:43 -0300

    Funcionando adicionar texto e foto

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 var loadFile = function(event) {
-    var output = document.getElementById('meme-image');
+    var output = document.getElementById('imagem-capturada');
     output.src = URL.createObjectURL(event.target.files[0]);
-  };
+};
+
+function mostraTexto(){
+    let textoDigitado = document.getElementById("text-input").value;
+    let inserir=document.createElement("p");
+    inserir.innerText=textoDigitado;
+    let textoAnterior=document.getElementsByClassName("moldura")[0].getElementsByTagName("p")[0];
+    
+    if(textoAnterior){
+        document.getElementsByClassName("moldura")[0].removeChild(textoAnterior);
+    }
+
+    document.getElementsByClassName("moldura")[0].appendChild(inserir);
+}
