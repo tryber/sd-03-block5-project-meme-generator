73789335274c4d0b62d0f210e2837e9ad5697cc3
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-02-28 16:43:12 -0300

    Meme Generator primeiro push

---
+++ script.js
@@ -0,0 +1,12 @@
+let memetxt = document.getElementById('text-input');
+let inputtext = document.getElementById('input-text');
+let memeImage = document.getElementById('meme-image');
+
+let meme = function(event) {
+    let image = document.getElementById('imagem')
+    image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+memetxt.addEventListener('keyup', function() {
+    inputtext.innerHTML = this.value;
+});
