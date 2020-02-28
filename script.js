9e997db4c61b40438c5a7f5d60def41d0a28caf9
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-02-28 19:13:49 -0300

    Arrumando o CSS, JS e HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+var loadFile = function (event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+};
+
+let textBox = document.getElementById('text-input');
+textBox.addEventListener('input', function() {
+    let textImage = document.getElementById('meme-text');
+    textImage.innerHTML = textBox.value
+})
