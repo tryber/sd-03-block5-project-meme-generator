b304ae7a30868f14fb0b3369e1c44adc446102f6
tales <talestales@gmail.com>
2020-02-28 18:07:13 -0300

    qwe

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const inputFundo = document.querySelector("#meme-image");
-function colocaFoto() {
-    document.getElementsByClassName("caixa").background = inputFundo;
-                      }
 const caixaTexto = document.querySelector('#text-input');
 const caixa = document.querySelector('.caixa');
 function trocaTexto () {
   caixa.innerHTML = caixaTexto.value;
 }
 caixaTexto.addEventListener("keyup", trocaTexto);
+
+function previewFile() {
+  var preview = document.querySelector('img');
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "";
+  }
+}
