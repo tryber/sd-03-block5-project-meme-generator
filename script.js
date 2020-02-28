17c75b18831432fa85b4bb0dee95963a75636484
Thiago Tavares <thiagooo15@gmail.com>
2020-02-28 16:40:46 -0300

    caixa de mensagem

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("oi")
+let texto = document.getElementById("text-input")
+
+function escreveText() {
+  document.getElementById("meme-text").innerHTML = texto.value;
+}
+
+texto.addEventListener("keyup", escreveText);
