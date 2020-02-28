6e421942a3a1018d0d54079aace72091726e2616
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-28 18:00:01 -0300

    add memes

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 let textInput = document.getElementById("text-input");
-let imageInput = document.getElementById("input-image");
+let memeInsert = document.getElementById("meme-insert");
+
 let memeImage = document.getElementById("meme-image");
 let memeText = document.getElementById("meme-text");
+
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
 
+let meme1 = document.getElementById("meme1");
+let meme2 = document.getElementById("meme2");
+let meme3 = document.getElementById("meme3");
+
 function addMeme() {
-    memeImage.src = URL.createObjectURL(imageInput.files[0]);
+    memeImage.src = URL.createObjectURL(memeInsert.files[0]);
 }
 
-imageInput.addEventListener("change", addMeme);
+memeInsert.addEventListener("change", addMeme);
 
 function addText() {
-    memeText.innerHTML = textInput.value
+    memeText.innerHTML = textInput.value;
 }
 
 textInput.addEventListener("keyup", addText);
@@ -44,4 +50,32 @@ button2.addEventListener("click", function() {
 
 button3.addEventListener("click", function() {
     addBorder(3);
+});
+
+function selectMeme(indicador) {
+    if(indicador == 1) {
+        memeImage.src = meme1.src;
+    } else if(indicador == 2) {
+        memeImage.src = meme2.src;
+    } else if(indicador == 3) {
+        memeImage.src = meme3.src;
+    } else if (indicador == 4) {
+        memeImage.src = meme4.src;
+    }
+}
+
+meme1.addEventListener("click", function() {
+    selectMeme(1);
+});
+
+meme2.addEventListener("click", function() {
+    selectMeme(2);
+});
+
+meme3.addEventListener("click", function() {
+    selectMeme(3);
+});
+
+meme4.addEventListener("click", function() {
+    selectMeme(4);
 });
