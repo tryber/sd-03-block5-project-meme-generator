5843acaf9c84453b0b32e5b94649f8261e2a12c4
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-28 18:56:05 -0300

    add memes

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
 
+let meme1 = document.getElementById("meme1");
+let meme2 = document.getElementById("meme2");
+let meme3 = document.getElementById("meme3");
+let meme4 = document.getElementById("meme4");
+
 function addMeme() {
     memeImage.src = URL.createObjectURL(memeInsert.files[0]);
 }
@@ -32,6 +37,18 @@ function addBorder(indicador) {
     }
 }
 
+function selectMeme(indicador) {
+    if (indicador == 1) {
+        memeImage.src = meme1.src;
+    } else if (indicador == 2) {
+        memeImage.src = meme2.src;
+    } else if (indicador == 3) {
+        memeImage.src = meme3.src;
+    } else if (indicador == 4) {
+        memeImage.src = meme4.src;
+    }
+}
+
 memeInsert.addEventListener("change", addMeme);
 
 textInput.addEventListener("keyup", addText);
@@ -46,4 +63,20 @@ button2.addEventListener("click", function () {
 
 button3.addEventListener("click", function () {
     addBorder(3);
+});
+
+meme1.addEventListener("click", function () {
+    selectMeme(1);
+});
+
+meme2.addEventListener("click", function () {
+    selectMeme(2);
+});
+
+meme3.addEventListener("click", function () {
+    selectMeme(3);
+});
+
+meme4.addEventListener("click", function () {
+    selectMeme(4);
 });
