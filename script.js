433eaa078a3e704285159b5ea1074acb1da16e0e
potew <potew0@gmail.com>
2020-02-28 18:36:51 -0300

    Requisitos obrigatórios 3

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ caixaTextoSuperior = document.getElementById("text-input");
 caixaTextoInferior = document.getElementById("text-input2");
 textoDeBaixo = document.getElementById("meme-text2");
 textoDeCima = document.getElementById("meme-text");
+caixaCor = document.getElementById("seletorCor");
+
+caixaCor.addEventListener('change', function() {
+    textoDeCima.style.color = caixaCor.value;
+    textoDeBaixo.style.color = caixaCor.value;
+});
 
 function carregaImagem() {
-    let botCarrega = document.getElementById("abreArquivo");
+    let botCarrega = document.getElementById("meme-insert");
     let blobImagem = window.URL.createObjectURL(botCarrega.files[0]);
     let imagemCarregada = document.getElementById('meme-image');
     imagemCarregada.src = blobImagem;
-    imagemCarregada.style.width = blobImagem.width;
     insereTexto();
     }
-        
+
 function insereTexto() {
     textoDeCima.textContent = caixaTextoSuperior.value;
     textoDeBaixo.textContent = caixaTextoInferior.value;
