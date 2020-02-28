f7d6276b41a4e1c4b2bffa087d94bdbf12fc250e
Thiago Tavares <thiagooo15@gmail.com>
2020-02-28 19:27:27 -0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let texto = document.getElementById("text-input");
 let textOutput = document.getElementById("meme-text");
 let botao = document.getElementById("meme-insert");
+let imagem = document.getElementById("meme-imagem")
 
 texto.addEventListener("keyup", escreveText);
 
 function escreveText() {
   textOutput.innerHTML = texto.value;
 }
+
+// function botaImg(){
+//   botao.onclick
+// }
