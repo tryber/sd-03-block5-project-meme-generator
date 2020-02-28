6481fbccc92f321f2a5022023f64de5893d4bb49
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-28 15:48:56 -0300

    add função de colocar a imagem

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-let memeText = document.getElementById("meme-text").innerHTML;
+let memeText = document.getElementById("meme-text");
+let memeImage = document.getElementById("meme-image");
+let lugarMeme = document.getElementById("lugarMeme");
 
-let memeImage = document.getElementById("meme-image").innerHTML;
+function addMeme() {
+    lugarMeme.src = URL.createObjectURL(memeImage.files[0]);
+    lugarMeme.style.width = 400 + "px";
+    lugarMeme.style.height = 400 + "px";
+}
+
+memeImage.addEventListener("change", addMeme);
