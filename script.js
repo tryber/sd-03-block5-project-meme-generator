a1c5eba085f7b2c706614ac0de81cff8777e9b00
luizfernando <luizfernandotic@gmail.com>
2020-02-28 19:32:38 -0300

    corrigindo codeclimate tentativa 2

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let loadFile = function (event) {
-   let image = document.getElementById('meme-image');
-   image.src = URL.createObjectURL(event.target.files[0]);
+    let image = document.getElementById('meme-image');
+    image.src = URL.createObjectURL(event.target.files[0]);
 }
 
 let textoDoMeme = document.getElementById("text-input");
 
 
 function textonaimagem() {
-   document.getElementById('meme-text').innerHTML = textoDoMeme.value;
+    document.getElementById('meme-text').innerHTML = textoDoMeme.value;
 }
-
-
 
 textoDoMeme.addEventListener("keyup", textonaimagem);
