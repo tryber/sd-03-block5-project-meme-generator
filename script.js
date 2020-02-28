f8740d33a11365de9532a60f002837cf3b7e28a3
tales <talestales@gmail.com>
2020-02-28 19:27:51 -0300

    asdaasd

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ function previewFile() {
     preview.src = "";
   }
 }
+
+
+    let corA = document.getElementsByClassName("a").border;
+    let corB = document.getElementsByClassName("b").border;
+    let corC = document.getElementsByClassName("c").border;
+    let borda = document.getElementsByClassName("caixa").border;
+    function trocaBordaA(){
+        borda = corA;
+    }
+    function trocaBordaB(){
+        borda = corB;
+    }
+    function trocaBordaC(){
+        borda = corC;
+    }
+
+    let meme1 = document.querySelector('#meme1');
+    let meme2 = document.querySelector('#meme2');
+    let meme3 = document.querySelector('#meme3');
+    let meme4 = document.querySelector('#meme4');
+
+    meme1.addEventListener("click", function(){})
