ef42ad034d6a4e0c54816fc7e4e744008ab990d9
Gabriel Lucas <dreadpool38@gmail.com>
2020-02-28 18:36:45 -0300

    Update para os objetivos principais

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 
+  //Muda o placeholder da imagem pela imagem do input
+  let input = document.getElementById("meme-image");
 
-let input = document.getElementById("inputImage");
+input.addEventListener('change',function () {
+    let inputImg = input.files[0];
+    let reader = new FileReader();
+    let imagePH = document.getElementById("imagemPH");
 
-input.addEventListener('change',function ()
-{alert("Foi adicionado");
-  let imagemMeme = input.files[0];
-  let imagemPH = document.getElementById("meme-image");
-  let reader = new FileReader();
-
-  if (imagemMeme){
-    reader.readAsDataURL(imagemMeme);
-  }
-
-  reader.onloadend = function(){
-    imagemPH.src = reader.result;
-    console.log(reader.result)
-  };
+    if (inputImg){
+      reader.readAsDataURL(inputImg)
+      console.log("lido");
+    }
+    reader.onloadend = function ()
+    {
+      imagePH.src = reader.result;
+    }
+  })
 
 
-})
+  //Coloca o texto sobre do meme sobre a imagem
+  let textInput = document.getElementById("text-input");
+
+  textInput.addEventListener('change',function (e) {
+    let texto = e.target.value;
+
+    let textPH = document.getElementById("input-text");
+
+    textPH.innerHTML = texto;
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
