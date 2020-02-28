f4c0a7045f4aac91bef4d74ef54da8a142250bb4
ilan274 <ilan.herbach@gmail.com>
2020-02-28 19:18:16 -0300

    Verify code climate requirements

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get input and paste into the page
+document.querySelector("#text-input").addEventListener('input', function (event) {
+  document.querySelector(".meme-text").innerHTML = document.querySelector("#text-input").value;
+})
+
+var loadFile = function (event) {
+  var image = document.getElementById('meme-image');
+  image.src = URL.createObjectURL(event.target.files[0]);
+}
