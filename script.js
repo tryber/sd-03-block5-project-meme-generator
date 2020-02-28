7cdb162f98d3f25c7a946ebc4dacc6c84cfd5cce
alcantara2509 <alcantara2509@gmail.com>
2020-02-28 19:50:18 -0300

    requisitos opcionais

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let text_input = document.querySelector('#text-input');
+let text_input2 = document.querySelector('#text-input2');
 let meme_text = document.querySelector('#meme-text');
+let meme_text2 = document.querySelector('#meme-text2');
 let meme_image = document.querySelector('#meme-image');
 let meme_insert = document.querySelector('#meme-insert');
 
-text_input.addEventListener('input', function changeText(){
+text_input.addEventListener('input', function() {
   meme_text.innerHTML = text_input.value
+})
+
+text_input2.addEventListener('input', function() {
+  meme_text2.innerHTML = text_input2.value
 })
 
 meme_insert.addEventListener('change', function() {
