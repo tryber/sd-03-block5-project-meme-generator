4630cf91d9dbaf771cbb649b40431a8aee491db4
Gabriel Luiz <gabrielsilva.adm@gmail.com>
2020-02-28 19:06:10 -0300

    Atualizando tags novamente
    atualizando tags novamente

--- script.js
+++ script.js
@@ -6,28 +6,15 @@ input1.addEventListener('keyup', escreveImagem);
 function escreveImagem() {
     var texto = input1.value;
     memeText.innerHTML = texto;
-
-    //console.log('batata')
-}
-
-function testando(){
-    console.log(test)
+s
 }
 
 inputImg.addEventListener('change', readURL);
 function readURL() {
     var file = inputImg.files[0];
     var reader = new FileReader();
+    reader.readAsDataURL(file);
     reader.onloadend = function () {
         document.querySelector('#meme-image').style.backgroundImage = "url(" + reader.result + ")";
-
-    }
-    if (file) {
-        reader.readAsDataURL(file);
-    } else {
-        console.log('nao deu')
     }
 }
-
-var test = document.querySelector('#text-input').value;
-console.log(test);
