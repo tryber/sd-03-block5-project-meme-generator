ac268b7a45f904dd49c63665e83274211802190b
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-28 16:07:18 -0300

    Terminando funçao que faz upload da imagem

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 window.onload = () => {
-  const changeImg = () => {
-    console.log(document.getElementById("image-input").value);
-    document.getElementById("meme-image").src = document.getElementById("image-input").value
+  document.getElementById("image-input").addEventListener("change", (event) => {
+    document.getElementById('meme-image').src = URL.createObjectURL(event.target.files[0]);
     document.getElementById("meme-image").style.borderColor = "black";
     document.getElementById("meme-image").style.borderWidth = "1px";
     document.getElementById("meme-image").style.borderStyle = "solid";
     document.getElementById("meme-image").style.backgroundColor = "white";
-  };
-  document.getElementById("image-input").addEventListener("change", changeImg)
+  } )
 };
+
+
+
