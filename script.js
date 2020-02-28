567401e8cfb7f239c81d69c4a80ca3534454ed55
Lucas França <luc.rangel90@gmail.com>
2020-02-28 18:44:03 -0300

    primeiro pull request

---
+++ script.js
@@ -0,0 +1,16 @@
+let textoForm = document.querySelector('#text-input');
+let textoCaixa = document.querySelector('.textoNaImagem');
+
+function colocaTextoCaixa() {
+  textoCaixa.innerText = textoForm.value;
+}
+
+textoForm.addEventListener('keyup', colocaTextoCaixa);
+
+let recebeFoto = document.querySelector('.imagemUnica');
+let botaoFoto = document.querySelector('#botaoFoto');
+
+function loadImage(event) {
+  recebeFoto.src = window.URL.createObjectURL(event.target.files[0]);
+}
+botaoFoto.addEventListener('change', loadImage);
