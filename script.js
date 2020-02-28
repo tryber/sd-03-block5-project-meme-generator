b2f0f2435bb5507bf5333f398d5718d1aea54f4e
Lourenço <loulage@gmail.com>
2020-02-28 16:56:41 -0300

    Implemented uploadImage Function and Text Function

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+    let inputText = document.getElementById('text-input')
+    let inputImage = document.getElementById('image-input');
+    let p = document.getElementById('meme-text');
+
+    function uploadImage(event) {
+        let image = document.getElementById('meme-image');
+        image.src = URL.createObjectURL(event.target.files[0]);
+    };
+    inputImage.addEventListener("change", uploadImage);
+
+    function showText() {
+        p.innerHTML = inputText.value;
+    }
+    inputText.addEventListener("keydown", showText);
+};
