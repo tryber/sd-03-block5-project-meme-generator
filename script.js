502c06060584261641a9e3c589a3e27e71e0d842
ilan274 <ilan.herbach@gmail.com>
2020-02-29 17:42:43 -0300

    Verify project requirements

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ var loadFile = function (event) {
   var image = document.getElementById('meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
 }
+
+// Own style buttons
+const getButton1 = document.getElementById("button1")
+const getButton2 = document.getElementById("button2");
+const getButton3 = document.getElementById("button3");
+const getDiv = document.querySelector("div");
+
+// Event listeners
+getButton1.addEventListener('click', button1)
+getButton2.addEventListener('click', button2)
+getButton3.addEventListener('click', button3)
+
+// Button1 function
+function button1() {
+  getDiv.style.border = "3px dashed red";
+}
+
+// Button2 function
+function button2() {
+  getDiv.style.border = "5px double blue"
+}
+
+// Button3 function
+function button3() {
+  getDiv.style.border = "6px groove green"
+}
