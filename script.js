242cab346b04456b220157c98f4d02f1377fdb64
davidepadua <davidepadua1914@gmail.com>
2020-02-29 12:45:06 -0300

    Fonte adicionada

---
+++ script.js
@@ -0,0 +1,25 @@
+function preview(event) {
+    var reader = new FileReader();
+    var campo = document.querySelector('.imagem-selecionada');
+
+    reader.onload = function() {
+        if (reader.readyState == 2) {
+            campo.src = reader.result;
+        }
+
+    }
+    reader.readAsDataURL(event.target.files[0]);
+
+}
+let textTop = document.querySelector('#texto-cima');
+console.log(textTop);
+
+function overlaidTop() {
+    let text = document.querySelector('#texto-cima').value;
+    document.querySelector('#top-phrase').innerHTML = text;
+}
+
+function overlaidBot() {
+    let text = document.querySelector('#texto-inferior').value;
+    document.querySelector('#bottom-phrase').innerHTML = text;
+}
