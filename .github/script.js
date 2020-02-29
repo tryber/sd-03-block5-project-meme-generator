ac2f53411a867451ebae91f073cefeba6de2c4da
DiegoRafaelAF <draf0314@gmail.com>
2020-02-28 23:27:05 -0300

    Atualizando as Informaçoes das TAGs

--- .github/script.js
+++ .github/script.js
@@ -1,10 +1,10 @@
 window.onload = function () {
-  let addImage = document.getElementById("meme-insert");
+  let addImage = document.getElementById("meme-image");
   let addText = document.getElementById("text-input");
   let memeText = document.getElementById("meme-text");
 
   function criandoUrl(event) {
-    let image = document.getElementById("meme-image");
+    let image = document.getElementById("meme-image-container");
     image.src = URL.createObjectURL(event.target.files[0]);
   };
   addImage.addEventListener("change", criandoUrl);
