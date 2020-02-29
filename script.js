956edc7446591a4a34c139071ae1935676b47298
fabemiliano <fab.emiliano@gmail.com>
2020-02-29 16:32:51 -0300

    Added borders

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ let image = document.getElementById('meme-image');
 function uploadImage() {
     image.src = URL.createObjectURL(event.target.files[0]);
 }
+
+
+//mudando as bordas
+
+let button1 = document.getElementById("button1")
+let button2 = document.getElementById("button2")
+let button3 = document.getElementById("button3")
+
+
+button1.addEventListener("click",changeButton1)
+button2.addEventListener("click",changeButton2)
+button3.addEventListener("click",changeButton3)
+
+function changeButton1(){
+    document.getElementById("meme-image-container").style.border = "3px dashed red"
+}
+
+function changeButton2(){
+    document.getElementById("meme-image-container").style.border = "5px double blue"
+}
+
+function changeButton3(){
+    document.getElementById("meme-image-container").style.border = "6px groove green"
+}
+
+
+
+
