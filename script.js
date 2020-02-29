c65f57d103e1502f47453a682331835246c0c4d3
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-29 13:01:12 -0300

    teste

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ window.onload = () => {
   document.getElementById("button1").addEventListener("click", () => document.getElementById("meme-image-container").style.border = "3px dashed red");
   document.getElementById("button2").addEventListener("click", () => document.getElementById("meme-image-container").style.border ="5px double blue");
   document.getElementById("button3").addEventListener("click", () => document.getElementById("meme-image-container").style.border = "6px groove green");
-  document.getElementById("meme-1").addEventListener("click", () => document.getElementById("meme-image").src = "imgs/meme-1.jpeg");
-  document.getElementById("meme-2").addEventListener("click", () => document.getElementById("meme-image").src = "imgs/meme-2.jpeg");
-  document.getElementById("meme-3").addEventListener("click", () => document.getElementById("meme-image").src = "imgs/meme-3.jpeg");
-  document.getElementById("meme-4").addEventListener("click", () => document.getElementById("meme-image").src = "imgs/meme-4.jpeg");
+  document.getElementById("meme-1").addEventListener("click", () => document.getElementById("meme-image").src = "imgs/meme1.jpeg");
+  document.getElementById("meme-2").addEventListener("click", () => document.getElementById("meme-image").src = "imgs/meme2.jpeg");
+  document.getElementById("meme-3").addEventListener("click", () => document.getElementById("meme-image").src = "imgs/meme3.jpeg");
+  document.getElementById("meme-4").addEventListener("click", () => document.getElementById("meme-image").src = "imgs/meme4.jpeg");
 };
