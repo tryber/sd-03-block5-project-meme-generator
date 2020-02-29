98c40ec242a7260ee2cf559ad6f2a46f80b32fb2
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-28 21:24:50 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 window.onload = () => {
   document.getElementById("meme-insert").addEventListener("change", (event) => {
     document.getElementById('meme-image').src = URL.createObjectURL(event.target.files[0]);
-    // document.getElementById("meme-image").style.borderColor = "black";
-    // document.getElementById("meme-image").style.borderWidth = "1px";
-    // document.getElementById("meme-image").style.borderStyle = "solid";
-    // document.getElementById("meme-image").style.backgroundColor = "white";
   } )
 
   document.getElementById("text-input").addEventListener("keyup", (event) => {
