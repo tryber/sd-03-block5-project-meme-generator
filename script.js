926ecb6aea1b9331d2a1cb1caf6380913ae99782
Marylange <marylangess@gmail.com>
2020-02-29 17:39:55 -0300

    teste

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // let memeImage = document.querySelector('#meme-image');
 // let memeInsert = document.querySelector('#meme-insert');
-var loadFile = function (event) {
-  var image = document.getElementById('meme-image');
+let loadFile = function (event) {
+  let image = document.getElementById('meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+function escreveCaixaImagem() {
+  let caixaTexto = document.getElementById('text-input');
+  let tagP = document.getElementById('texto-imagem');
+  tagP.innerText = caixaTexto.value;
+}
