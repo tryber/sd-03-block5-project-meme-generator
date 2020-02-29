35c492bdc2f2122451494b656ee0b57f2b4434f0
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-02-29 01:46:38 -0300

    correção id

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let loadFile = function(event) {
+let loadFile = function (event) {
     let output = document.getElementById('meme-image');
     output.src = URL.createObjectURL(event.target.files[0]);
 };
 
-function mostraTexto(){
-    let textoDoUsuario= document.getElementById("text-input").value;
-    let inserir=document.createElement("p");
-    inserir.innerText=textoDoUsuario;
-    let texto=document.getElementsByClassName("posicaoDaImagem")[0].getElementsByTagName("p")[0];
+function mostraTexto() {
+    let textoDoUsuario = document.getElementById("text-input").value;
+    let inserir = document.createElement("p");
+    inserir.innerText = textoDoUsuario;
+    let texto = document.getElementsByClassName("posicaoDaImagem")[0].getElementsByTagName("p")[0];
 
-    if(texto){
+    if (texto) {
         document.getElementsByClassName("posicaoDaImagem")[0].removeChild(texto);
     }
 
