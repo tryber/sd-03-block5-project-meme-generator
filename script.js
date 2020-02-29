4c0113c9e51c9931fc63016bced35b598337cb97
Thiago Tavares <thiagooo15@gmail.com>
2020-02-28 21:36:42 -0300

    sem js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let texto = document.getElementById("text-input");
-let textOutput = document.getElementById("meme-text");
-let botao = document.getElementById("meme-insert");
-let imagem = document.getElementById("meme-imagem")
+// let texto = document.getElementById("text-input");
+// let textOutput = document.getElementById("meme-text");
+// let botao = document.getElementById("meme-insert");
+// let imagem = document.getElementById("meme-imagem")
 
-texto.addEventListener("keyup", escreveText);
+// // texto.addEventListener("keyup", escreveText);
 
-function escreveText() {
-  textOutput.innerHTML = texto.value;
-}
-
-// function botaImg(){
-//   botao.onclick
+// function escreveText() {
+//   textOutput.innerHTML = texto.value;
 // }
-// vamos fazer mais, ou chorar, o que for primeiro
+
+// // function botaImg(){
+// //   botao.onclick
+// // }
+// // vamos fazer mais, ou chorar, o que for primeiro
