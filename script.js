1ca6fbab455bdb612955ae3556dc62c09f36c1ca
Thiago Tavares <thiagooo15@gmail.com>
2020-02-29 01:31:52 -0300

    final principal vai!

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let texto = document.getElementById("text-input");
 let textOutput = document.getElementById("meme-text");
 let image = document.getElementById("meme-image");
-// let imgDiv = document.getElementById('meme-image-container');
+let imgDiv = document.getElementById('meme-image-container'); //mudar nomde desse let?!
+// adicionar lets pro botões
 
 texto.addEventListener("keyup", escreveText);
+// adicionar chamadas de funções por clicks dos botões
 
 function escreveText() {
   textOutput.innerHTML = texto.value;
@@ -12,3 +14,5 @@ function escreveText() {
 function loadImage() {
     image.src = URL.createObjectURL(event.target.files[0]);
 }
+
+// adicionar funções pros clicks dos botões
