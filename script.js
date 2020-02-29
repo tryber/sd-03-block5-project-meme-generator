5fa6940a28c57ea43da6220e075e99a8394ad943
rogerfaustino <roger_faustino@hotmail.com>
2020-02-29 11:05:08 -0300

    Starting the meme-generator project

--- script.js
+++ script.js
@@ -1,44 +1,9 @@
 window.onload = function() {
-	previewFiles();
-	textInput();
-}
-
-function previewFiles() {
-
-  let preview = document.querySelector('#preview');
-  let files   = document.getElementById('meme-image').files;
-// let files   = document.querySelector('input[type=file]').files;
-
-  function readAndPreview(file) {
-    let reader = new FileReader();
-    
-    reader.addEventListener("load", function () {
-    let image = new Image();
-		image.height = 512;
-		image.width = 512;
-    image.title = file.name;
-    image.src = this.result;
-    preview.appendChild( image );
-    }, false);
-
-      reader.readAsDataURL(file);
-  }
-
-  if (files) {
-    [].forEach.call(files, readAndPreview);
-  }
-}
-
-function textInput(topText, bottomText) {
-
-  let topTextInput = document.querySelector('#meme-text');
-  let bottomTextInput = document.querySelector('#text-input');
-
-  topTextInput.addEventListener("keyup", function() {
-    textInput = topTextInput.innerHTML;
-    console.log(topTextInput);
-    textInput.appendChild( preview );
-
+  document.getElementById('meme-insert').addEventListener('change', (event) => {
+    document.getElementById('meme-image').src = URL.createObjectURL(event.target.files[0]);
   })
-  
-}
+
+  document.getElementById('text-input').addEventListener('keyup', (event) => {
+    document.getElementById('meme-text').innerHTML = event.target.value;
+  })
+};
