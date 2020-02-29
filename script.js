4b0f6928d9dc67bb254646f3fa08e170c9d1b646
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-29 11:11:27 -0300

    Tentando fazer funcionar o primeiro requisito 27

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function mostraTexto(){
     let textoDigitado = document.getElementById("text-input").value;
     let inserir=document.createElement("p");
     inserir.innerText=textoDigitado;
-    
+
     let textoAnterior=document.getElementsByClassName("moldura")[0].getElementsByTagName("p")[0];
 
     if(textoAnterior){
