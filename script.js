f21fa7753888e442ecf663f62f15148548f0cdf3
Marylange <marylangess@gmail.com>
2020-02-29 01:44:19 -0300

    verificando requisitos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // let memeImage = document.querySelector('#meme-image');
 // let memeInsert = document.querySelector('#meme-insert');
 
-var loadFile = function(event) {
-	var image = document.getElementById('meme-image');
-	image.src = URL.createObjectURL(event.target.files[0]);
+var loadFile = function(event){
+    var image = document.getElementById('meme-image');
+    image.src = URL.createObjectURL(event.target.files[0]);
 };
