f01dd7a52120140acbf7c714d464b3df751ad68c
Thiago Tavares <thiagooo15@gmail.com>
2020-02-28 22:18:12 -0300

    100% so que nao

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// let texto = document.getElementById("text-input");
-// let textOutput = document.getElementById("meme-text");
-// let botao = document.getElementById("meme-insert");
-// let imagem = document.getElementById("meme-imagem")
+let texto = document.getElementById("text-input");
+let textOutput = document.getElementById("meme-text");
+let botao = document.getElementById("meme-insert");
+let imagem = document.getElementById("meme-imagem");
 
-// // texto.addEventListener("keyup", escreveText);
+texto.addEventListener("keyup", escreveText);
 
-// function escreveText() {
-//   textOutput.innerHTML = texto.value;
-// }
+function escreveText() {
+  textOutput.innerHTML = texto.value;
+}
 
-// // function botaImg(){
-// //   botao.onclick
-// // }
-// // vamos fazer mais, ou chorar, o que for primeiro
+let loadFile = function(event) {
+  let image = document.getElementById("meme-image");
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
