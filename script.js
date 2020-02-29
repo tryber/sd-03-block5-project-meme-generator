fecdaffc838f7e64ef2cade08d516149a522b92d
Felipe Gomes <contato.felipeluna@gmail.com>
2020-02-29 10:23:54 -0300

    test coment js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let mostraFoto = function(event) {
-    let imagem = document.getElementById('meme-image');
-    imagem.src = URL.createObjectURL(event.target.files[0]);
-  };
+// let mostraFoto = function(event) {
+//     let imagem = document.getElementById('meme-image');
+//     imagem.src = URL.createObjectURL(event.target.files[0]);
+//   };
 
-let texto = document.getElementById("text-input");
-texto.addEventListener('input', exibir);
+// let texto = document.getElementById("text-input");
+// texto.addEventListener('input', exibir);
 
-function exibir(){
-  document.getElementById("meme-text").innerHTML = texto.value;
-}
+// function exibir(){
+//   document.getElementById("meme-text").innerHTML = texto.value;
+// }
