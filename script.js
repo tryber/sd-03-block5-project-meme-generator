e07765eab56675ff10f4660b47d3d6e602a0cbbd
DevT <aguiarthiag@gmail.com>
2020-02-29 17:48:12 -0300

    tentando bônus da borda

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ window.onload = function(){
     let textInput = document.getElementById('text-input');
     let imagemInput = document.getElementById('meme-insert');
     let memeTxt = document.getElementById("meme-text");
+    let btn1 = document.getElementById("button-1")
+    let btn2 = document.getElementById("button-2")
+    let btn3 = document.getElementById("button-3")
    
     function memeTxtInput(){
         memeTxt.innerText = textInput.value;
@@ -18,5 +21,33 @@ window.onload = function(){
     
     }
     imagemInput.addEventListener('change', upMemeImage);
+    
+
+    function border1 (){
+        let container = document.getElementById("meme-image-container")
+        container.style.borderWidth = "3px"
+        container.style.borderStyle = "dashed"
+        container.style.borderColor = "red"
     }
 
+    btn1.addEventListener('click', border1)
+
+    function border2 (){
+        let container = document.getElementById("meme-image-container")
+        container.style.borderWidth = "5px"
+        container.style.borderStyle = "double"
+        container.style.borderColor = "blue"
+    }
+
+    btn2.addEventListener('click', border2)
+
+    function border3 (){
+        let container = document.getElementById("meme-image-container")
+        container.style.borderWidth = "6px"
+        container.style.borderStyle = "groove"
+        container.style.borderColor = "green"
+    }
+
+    btn3.addEventListener('click', border3)
+
+}
