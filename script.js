d9feb97de7b39b23e4efb7baba2d1da9a35376bb
Marylange <marylangess@gmail.com>
2020-02-28 23:04:24 -0300

    teste dos requisitos

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let memeImage = document.querySelector('#meme-image');
-let memeInsert = document.querySelector('#meme-insert');
-function lerImage(){
-    
-}
+// let memeImage = document.querySelector('#meme-image');
+// let memeInsert = document.querySelector('#meme-insert');
+
+var loadFile = function(event) {
+	var image = document.getElementById('meme-image');
+	image.src = URL.createObjectURL(event.target.files[0]);
+};
