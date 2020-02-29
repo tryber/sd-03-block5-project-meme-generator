71e91703057705d8f9749ad16ca2e8e61ff4b75c
DiegoRafaelAF <draf0314@gmail.com>
2020-02-29 00:25:31 -0300

    Corrigindo TagS

--- .github/script.js
+++ .github/script.js
@@ -1,11 +1,13 @@
 window.onload = function () {
-  let addImage = document.getElementById("meme-image");
+  let addImage = document.getElementById("meme-insert");
   let addText = document.getElementById("text-input");
   let memeText = document.getElementById("meme-text");
 
   function criandoUrl(event) {
-    let image = document.getElementById("meme-image-container");
+    let image = document.getElementById("meme-image");
     image.src = URL.createObjectURL(event.target.files[0]);
+    image.style.height = 400 + "px";
+    image.style.width = 400 + "px";
   };
   addImage.addEventListener("change", criandoUrl);
 
