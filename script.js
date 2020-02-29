979295979013cfc109f21665fbe6dd3fad8c14cc
Felipe Gomes <contato.felipeluna@gmail.com>
2020-02-29 16:55:12 -0300

    req04_16.55

--- script.js
+++ script.js
@@ -6,6 +6,35 @@ let mostraFoto = function(event) {
 let texto = document.getElementById("text-input");
 texto.addEventListener('input', exibir);
 
+let botao1 = document.getElementById("button1");
+botao1.addEventListener('click', borda1);
+
+let botao2 = document.getElementById("button2");
+botao2.addEventListener('click', borda2);
+
+let botao3 = document.getElementById("button3");
+botao3.addEventListener('click', borda3);
+
+borda = document.getElementById("meme-image-container");
+
 function exibir(){
   document.getElementById("meme-text").innerHTML = texto.value;
+}
+
+function borda1(){
+    borda.style.borderColor = "red";
+    borda.style.borderStyle = "dashed";
+    borda.style.borderWidth = "3px";
+}
+
+function borda2(){
+    borda.style.borderColor = "blue";
+    borda.style.borderStyle = "double";
+    borda.style.borderWidth = "5px";
+}
+
+function borda3(){
+    borda.style.borderColor = "green";
+    borda.style.borderStyle = "groove";
+    borda.style.borderWidth = "6px";
 }
