ad164e4fdc2c9b19e1f8dc143445056c5a4afcd8
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-03-01 20:09:01 -0300

    correção css

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let texto = document.getElementById("text-input");
-let memeText = document.getElementById("meme-text");
-texto.addEventListener("keyup", writeText);
-let selectedPic = document.getElementById("meme-image");
 let imageContainer = document.getElementById("image-container");
+let memeText = document.getElementById("meme-text");
+let selectedPic = document.getElementById("meme-image");
+let texto = document.getElementById("text-input");
 
 function setImage() {
   selectedPic.src = URL.createObjectURL(event.target.files[0]);
 }
+
 function writeText() {
   memeText.innerHTML = texto.value;
 }
+
+texto.addEventListener("keyup", writeText);
