51feb20a98f0a70588caafeb8bb2ba2c17b9fbd2
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-01 18:25:44 -0300

    bordas

--- script.js
+++ script.js
@@ -13,4 +13,24 @@ window.onload= function(){
     };
     let im = document.getElementById('meme-insert');
     im.addEventListener("change", loadFile)
+
+    let container= document.getElementById("meme-image-container")
+
+    function TrocaBorda1() {
+        container.style.border= "3px dashed red"
+    }
+    let bt1= document.getElementById("button1")
+    bt1.addEventListener("click", TrocaBorda1)
+
+    function TrocaBorda2() {
+        container.style.border= "5px double blue"
+    }
+    let bt2= document.getElementById("button2")
+    bt2.addEventListener("click", TrocaBorda2)
+
+    function TrocaBorda3() {
+        container.style.border= "6px groove green"
+    }
+    let bt3= document.getElementById("button3")
+    bt3.addEventListener("click", TrocaBorda3)
 }
