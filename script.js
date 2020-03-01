2c70065a5299ece50c21df054c198aa735dcb1e0
natox <alvares.renato@hotmail.com>
2020-02-29 21:04:09 -0300

    Change img to meme-image and button fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let loadFile = function (event) {
-  let image = document.getElementById('loadedImage');
+  let image = document.getElementById('meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
 
 }
@@ -10,23 +10,17 @@ let memeText = document.getElementById('memeText');
 function textInsert() {
   memeText.textContent = textInput.value
 }
-/* let button1 = document.getElementById('button1');
+let button1 = document.getElementById('button-1');
 button1.addEventListener('click', function () {
   document.getElementById('meme-image-container').style.border = '3px dashed red'
 })
-let button2 = document.getElementById('button2');
+let button2 = document.getElementById('button-2');
 button2.addEventListener('click', function () {
   document.getElementById('meme-image-container').style.border = '5px double blue'
 })
-let button2 = document.getElementById('button2');
-button2.addEventListener('click', function () {
-  document.getElementById('meme-image-container').style.border = '5px double blue'
-})
-let button2 = document.getElementById('button2');
-button2.addEventListener('click', function () {
-  document.getElementById('meme-image-container').style.border = '5px double blue'
-})
-let button3 = document.getElementById('button3');
+
+
+let button3 = document.getElementById('button-3');
 button3.addEventListener('click', function () {
   document.getElementById('meme-image-container').style.border = '6px groove green'
-}) */
+})
