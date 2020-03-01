87580a66e768c0362e577d7f2287fb9c9e733394
wscatao <wscatao@gmail.com>
2020-03-01 12:03:25 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -9,18 +9,17 @@ console.log(fileInput)
 
 // Eventos
 
-textInput.addEventListener("input",escreverTexto)
+textInput.addEventListener("input", escreverTexto)
 fileInput.addEventListener("change", carregaImagem)
 
 // Funções
 
-function escreverTexto(){
-    texto.innerHTML=textInput.value
+function escreverTexto() {
+  texto.innerHTML = textInput.value
 }
 
-function carregaImagem(){
-    let arquivo = this.files[0]
-    let img = document.getElementById("meme-image")
-    img.src = URL.createObjectURL(arquivo)
-    // img[0].src = URL.createObjectURL(arquivo)
+function carregaImagem() {
+  let arquivo = this.files[0]
+  let img = document.getElementById("meme-image")
+  img.src = URL.createObjectURL(arquivo)
 }
