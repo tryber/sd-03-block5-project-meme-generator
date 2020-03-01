a8170e20b4a17de263337cb4ea8c9a480a37ee6a
Ga Pimentel <cadgap@gmail.com>
2020-03-01 14:10:29 -0300

    Primeira tentativa de cumprir os requisitos 2 e 3, incluii o upload da imagem e a borda.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function inserirTexto() {
     let texto = document.getElementById("text-input").value;
     document.getElementById("meme-text").innerHTML = texto;
 }
+function visualizarImagem() {
+    let memeImage = document.getElementById("meme-image");
+    let reader = new FileReader();
+    reader.onload = function inicioReader(){
+        let dataURL = reader.result;
+        let memeInsert = document.getElementById("meme-insert");
+        memeInsert.src = dataURL;
+    }
+    reader.readAsDataURL(memeImage.files[0]);
+}
