5925446b6fccbef040049c848db5e2e90903ada7
Clayton Pereira <cpereiramt@gmail.com>
2020-03-01 12:46:41 -0400

    Acrescentando as opçoes para que o usuario possa mudar a aparencia da borda

--- script.js
+++ script.js
@@ -9,4 +9,20 @@ document.getElementById("meme-insert").onchange = function() {
      console.log(this.files[0]);
      let memeImagem = document.getElementById("meme-image");
      memeImagem.setAttribute("src",URL.createObjectURL(this.files[0]));
-     }
+}
+
+document.getElementById("button1").addEventListener("click",function () {
+   let imageContainer = document.getElementById("meme-image-container");
+   imageContainer.style.border="3px dashed red";    
+})
+
+document.getElementById("button2").addEventListener("click",function () {
+    let imageContainer = document.getElementById("meme-image-container");
+    imageContainer.style.border="5px dashed blue";
+    imageContainer.style.borderStyle="double"    
+ })
+
+document.getElementById("button3").addEventListener("click",function () {
+    let imageContainer = document.getElementById("meme-image-container");
+    imageContainer.style.border="6px groove green";    
+ })
