4742f9e993a8b5cf296c2282909bfb831584e8c4
William Igor <williamigorsilva@gmail.com>
2020-03-01 11:18:46 -0300

    Inserindo texto ao meme

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-function insereTexto() {
-  const texto = document.getElementById("text-input").value;
-  texto.addEventListener('keyup',insereTexto);
-  document.innerHTML = texto.value;
+// primeiro coloco o texto na caixa
+/*
+window.onload = function(){
+  let addImagem = document.getElementById("meme-insert");
+  let addText = document.getElementById("text-input");
+  let memeText = document.getElementById("meme-text");
 
+  // coloco imagem na caixa
+
+  
+    function insereTexto(input) {    
+      memeText.textContent = input.target.value;
+     };
+    addText.addEventListener("beforeinput",insereTexto);
+
+ 
 }
+*/
+window.onload = function () {
 
-document.querySelector("")
+  document.getElementById("text-input").addEventListener("keyup", function(){
+
+  
+    let texto = document.getElementById("insereTexto");
+  
+    if (this.value != ''){
+      texto.innerHTML = this.value;
+    }
+    else {
+      texto.innerHTML = "Esreva o texto";
+    }
+  
+  }) 
+
+  function uploadImage(event){
+    
+    document.getElementById("meme-image") = addEventListener("change", uploadImage);
+    let imagem = document.getElementById("meme-insert");
+
+    imagem.src = URL.createObjectURL(event.target.files[0]);
+  };  
+    
+
+
+};
