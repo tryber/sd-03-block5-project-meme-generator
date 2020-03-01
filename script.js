ab919040a7a99b1f66e60fa54e720120ca5cc816
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-03-01 16:01:26 -0300

    Added buttons config border

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 
 let textIn = document.getElementById("text-input");
-
 let textOut = document.getElementById("meme-text");
-
 let image = document.getElementById("meme-image");
+let butt1 = document.getElementById("button1");
+let butt2 = document.getElementById("button2");
+let butt3 = document.getElementById("button3");
+let imgChan = document.getElementsByClassName("image-box");
 
-textIn.addEventListener('keyup', changetxt);
+textIn.addEventListener("keyup", changetxt);
+butt1.addEventListener("click", border1);
+butt2.addEventListener("click", border2);
+butt3.addEventListener("click", border3);
 
-function changetxt() {
+function changetxt () {
     textIn.value.toUpperCase();
     textOut.innerHTML = textIn.value;
 }
 
-function loadImage() {
+function loadImage () {
     image.src = URL.createObjectURL(event.target.files[0]);
 }
  
+function border1 () {
+    imgChan.style.border = "3px dashed red"
+}
+
+function border2 () {
+    imgChan.style.border = "5px double blue"
+}
+
+function border3 () {
+    imgChan.style.border = "6px groove green"
+}
