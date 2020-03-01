8e92226404e3c381a47403678900e00c8462f0fe
Clayton Pereira <cpereiramt@gmail.com>
2020-03-01 11:59:51 -0400

    Salvando as alteraçoes na funcionalidade de mostrar o texto em cima da imagem

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ caixaTexto.addEventListener("input",function (event) {
     let memeTexto = document.querySelector("#meme-text");
     memeTexto.innerHTML= event.target.value;
 })
+
+document.getElementById("meme-insert").onchange = function() {
+     
+     console.log(this.files[0]);
+     let memeImagem = document.getElementById("meme-image");
+     memeImagem.setAttribute("src",URL.createObjectURL(this.files[0]));
+     }
