12cbafa2f6a47fe3e1505c41c30aa07f0d2b3de1
Felipe caetano <felipecaetano25.1996@gmail.com>
2020-03-02 16:35:07 -0300

    requisitos corrigidos

--- script.js
+++ script.js
@@ -12,16 +12,50 @@ function escreverNaImagem() {
  escreverNaImagem();
 
 
- function imagem(){
-     let arquivo = document.getElementById("meme-insert");
-     let bottom = document.getElementById("inserir");
-     let img = document.getElementById("meme-image");
-     bottom.addEventListener("click",function(s){
-        console.log(arquivo.value);
-       img.src = arquivo.value;
 
-     });
- }
- imagem();
 
   
+  let input = document.getElementById("meme-insert");
+
+input.addEventListener('change',function () {
+    let inputImagem = input.files[0];
+    console.log(inputImg);
+    let ler = new FileReader();
+    let image = document.getElementById("meme-image");
+    if (inputImagem){
+      ler.readAsDataURL(inputImg)
+
+    }
+    ler.onloadend = function ()
+    {
+      image.src = ler.result;
+    }
+  })
+
+
+
+  function botao1() {
+    let imagem = document.getElementById("meme-image-container");
+    imagem.style.borderStyle = "dashed";
+    imagem.style.borderColor = "red";
+    imagem.style.borderWidth = "3px";
+
+
+  }
+
+  function botao2() {
+    let imagem = document.getElementById("meme-image-container");
+    imagem.style.borderStyle = "double";
+    imagem.style.borderColor = "blue";
+    imagem.style.borderWidth = "5px";
+
+
+  }  function botao3() {
+    let imagem = document.getElementById("meme-image-container");
+    imagem.style.borderStyle = "groove";
+    imagem.style.borderColor = "green";
+    imagem.style.borderWidth = "6px";
+
+
+  }
+
