92a1d8bab4939dd1c405fe87e50c99dfae94e8bf
DiegoRafaelAF <draf0314@gmail.com>
2020-03-01 21:19:14 -0300

    Atualizando o requisito 4

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function () {
   let addText = document.getElementById("text-input");
   let memeText = document.getElementById("meme-text");
 
+
   function criandoUrl() {
     let image = document.getElementById("meme-image");
     image.src = URL.createObjectURL(addImage.files[0]);
@@ -16,3 +17,24 @@ window.onload = function () {
   };
   addText.addEventListener("beforeinput", inputText);
 }
+
+document.getElementById("button1").addEventListener("click", function () {
+  let imageContainer = document.getElementById("meme-image-container");
+  imageContainer.style.border = "3px";
+  imageContainer.style.border = "dashed";
+  imageContainer.style.border = "red";
+})
+
+document.getElementById("button2").addEventListener("click", function () {
+  let imageContainer = document.getElementById("meme-image-container");
+  imageContainer.style.border = "5px";
+  imageContainer.style.border = "double";
+  imageContainer.style.border = "blue";
+})
+
+document.getElementById("button2").addEventListener("click", function () {
+  let imageContainer = document.getElementById("meme-image-container");
+  imageContainer.style.border = "6px";
+  imageContainer.style.border = "double";
+  imageContainer.style.border = "green";
+})
