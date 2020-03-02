9eb123317b6579f0be82a797f9bab27d7a4ff431
PedroParreiras <pedro.bgparreiras@gmail.com>
2020-03-02 13:30:03 -0300

    ajustando js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const imagemescolhida = document.getElementById('meme-image').files[0];
+var digitando = document.getElementById('text-input')
+var escrevendo = document.getElementById('meme-text')
 
 
+digitando.addEventListener('input', function(){
+escrevendo.innerHTML = digitando.value
+})
+
+var imginsert = document.getElementById('meme-insert').files[0];
+var memeimage = document.getElementById('meme-image')
+
+imginsert.addEventListener('change', function(){
+memeimage.scr = URL.createObjectURL(imageinsert)
+}
