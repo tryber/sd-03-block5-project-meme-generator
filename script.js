884d8bcd58075781608e36d64842c2740628736c
Lourenço <loulage@gmail.com>
2020-03-02 11:35:17 -0300

    Fixed border buttons. Now target div meme-image-container

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.onload = function() {
     let inputImage = document.getElementById('meme-insert');
     let p = document.getElementById('meme-text');
     let image = document.getElementById('meme-image')
+    let imageContainer = document.getElementById('meme-image-container');
     let button1 = document.getElementById('button1');
     let button2 = document.getElementById('button2');
     let button3 = document.getElementById('button3');
@@ -23,17 +24,17 @@ window.onload = function() {
 
     //Border Buttons
     function borderButton1() {
-        image.style.border = "dashed red 3px";
+        imageContainer.style.border = "dashed red 3px";
     }
     button1.addEventListener('click', borderButton1);
 
     function borderButton2() {
-        image.style.border = "double blue 5px";
+        imageContainer.style.border = "double blue 5px";
     }
     button2.addEventListener('click', borderButton2);
 
     function borderButton3() {
-        image.style.border = "groove green 6px";
+        imageContainer.style.border = "groove green 6px";
     }
     button3.addEventListener('click', borderButton3);
 
