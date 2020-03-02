8503d32eb740bb5a161458854598e7b4e94efb8c
Bruno Wesley <bruno.wesley77@gmail.com>
2020-03-01 22:27:12 -0300

    update 2

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-window.onload = function() {
-    let memeInsert = document.getElementById("meme-insert");
-    let textInput = document.getElementById("text-input");
+window.onload = function(){
+    let textInput = document.getElementById('text-input');
+    let memeInsert = document.getElementById('meme-insert');
+    let memeText = document.getElementById('meme-text');
+    let memeImage = document.getElementById('meme-image');
+
+    function memeTextInput(){
+        memeText.innerText = textInput.value;
+    }
+    textInput.addEventListener('keyup', memeTextInput)
+
+    function imageInsert(){
+        memeImage.src = URL.createObjectURL(memeInsert.files[0]);
+    }
+    memeInsert.addEventListener('change', imageInsert);
 }
