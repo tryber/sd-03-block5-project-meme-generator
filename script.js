056aa0f6de2e0e81863dbd1a97076240b225ee71
Fernando Luckesi <fernando.luckesi94@gmail.com>
2020-03-02 16:46:15 -0300

    Botões com opções de borda da imagem inseridos e funcionando

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const memeImageContainer = document.getElementById('meme-image-container');
+const imageSpace = document.getElementById('meme-image');
 
 function adicionaImagem() {
   const imageInput = document.getElementById('meme-insert');
-  const imageSpace = document.getElementById('meme-image');
   imageInput.addEventListener('change', (event) => {
     imageSpace.src = URL.createObjectURL(event.target.files[0]);
     imageSpace.style.border = '1px solid black'
@@ -21,3 +21,23 @@ function adicionaFraseNaImagem() {
 }
 
 adicionaFraseNaImagem();
+
+function estilizarBorda() {
+  const tipo1 = document.getElementById('button1');
+  const tipo2 = document.getElementById('button2');
+  const tipo3 = document.getElementById('button3');
+
+  tipo1.addEventListener('click', () => {
+    memeImageContainer.style.border = '3px dashed red'
+  });
+
+  tipo2.addEventListener('click', () => {
+    memeImageContainer.style.border = '5px double blue'
+  });
+
+  tipo3.addEventListener('click', () => {
+    memeImageContainer.style.border = '6px groove green'
+  });
+}
+
+estilizarBorda();
