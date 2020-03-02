f7a700f5ec756a988ffb1f7e221141e415842222
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-02 13:45:48 -0300

    Formatting files

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var loadFile = function (event) {
-    var output = document.getElementById('meme-image');
-    output.src = URL.createObjectURL(event.target.files[0]);
+var loadFile = function(event) {
+  var output = document.getElementById("meme-image");
+  output.src = URL.createObjectURL(event.target.files[0]);
 };
 
-let textBox = document.getElementById('text-input');
-textBox.addEventListener('input', function() {
-    let textImage = document.getElementById('meme-text');
-    textImage.innerHTML = textBox.value
-})
+let textBox = document.getElementById("text-input");
+textBox.addEventListener("input", function() {
+  let textImage = document.getElementById("meme-text");
+  textImage.innerHTML = textBox.value;
+});
