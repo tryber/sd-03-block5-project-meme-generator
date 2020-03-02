a576f00ad27f2009210af9efc62a493ddf659987
Fernando Luckesi <fernando.luckesi94@gmail.com>
2020-03-02 04:17:55 -0300

    Projeto finalizado sem requisitos bonus version 1.2

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
+const memeImageContainer = document.getElementById('meme-image-container');
 
 function adicionaImagem() {
   const imageInput = document.getElementById('meme-insert');
   const imageSpace = document.getElementById('meme-image');
   imageInput.addEventListener('change', (event) => {
     imageSpace.src = URL.createObjectURL(event.target.files[0]);
+    memeImageContainer.style.border = '1px solid black'
   });
 }
 
 adicionaImagem();
 
-
-
 function adicionaFraseNaImagem() {
   const textSpace = document.getElementById('text-input');
-  const memeImageContainer = document.getElementById('meme-image-container');
   const fraseMemeNoParagrafo = document.getElementById('meme-text');
   memeImageContainer.appendChild(fraseMemeNoParagrafo);
   textSpace.addEventListener('keyup', () => {
