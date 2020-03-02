6c5be117d5ec4b8bc967908c39672cf5d9b53547
Gabriel Luiz <gabrielsilva.adm@gmail.com>
2020-03-02 15:05:22 -0300

    atualizacao de id´s

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var input1 = document.querySelector('#text-input');
-var inputImg = document.querySelector('#uploadInput')
+var inputImg = document.querySelector('#meme-insert')
 var memeText = document.querySelector('#meme-text');
 input1.addEventListener('keyup', escreveImagem);
 
 function escreveImagem() {
     var texto = input1.value;
     memeText.innerHTML = texto;
-s
 }
 
 inputImg.addEventListener('change', readURL);
