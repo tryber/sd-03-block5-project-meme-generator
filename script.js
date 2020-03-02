12c754d26d6c544bfcb4f3d94464b4703de43dc9
LulDelRey <rpgstrikered@gmail.com>
2020-03-02 16:25:20 -0300

    oh lord thank you

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-window.onload = startThePage();
-
-function startThePage() {
-  let imgText = document.querySelector("#text-input").value;
-  let imgMeme = document.querySelector("#meme-insert").value;
-  console.log(imgMeme);
-  console.log(imgText);
-}
 function changedImg() {
-  console.log(imgMeme);
+  let memeImg = document.querySelector("img");
+  let file = document.querySelector("input[type=file]").files[0];
+  let reader = new FileReader();
+  reader.onloadend = function() {
+    memeImg.src = reader.result;
+  };
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    memeImg.src = "";
+  }
 }
+
 function changedText() {
+  let imgText = document.querySelector("#text-input").value;
   console.log(imgText);
+  document.getElementById("meme-text").innerHTML = imgText;
 }
