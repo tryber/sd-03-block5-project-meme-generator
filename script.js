6fbfc440821e80ca2bfefb86233245159699020e
Luma Arruda <luma.oarruda@gmail.com>
2020-03-02 10:13:52 -0300

    reader de input de imagem

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 window.onload = function () {
+  let txt = document.getElementById('text-input');
+  let image = document.getElementById('uploadeImg');
+  let imageUrl = document.getElementById('uploadeUrl');
+  let memeImage = document.getElementById('meme-image');
+
+  let openFile = function(event) {
+      let input = event.target;
+      let reader = new FileReader();
+      reader.onload = function(){
+        let dataURL = reader.result;
+        memeImage.src = dataURL;
+      };    
+    reader.readAsDataURL(input.files[0]);
+  };
+  image.addEventListener('change', openFile)
 }
