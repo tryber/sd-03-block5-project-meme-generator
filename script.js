f30af25860a2336288359ecac45e4780ea95238e
paulo-lins <paulolins100@gmail.com>
2020-03-01 21:29:56 -0300

    quarto commit

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var textImg = document.querySelector('input#text-input');
 
-var loadFile =function(event) {
-    var output = document.querySelector('div#img');
-    var img = URL.createObjectURL(event.target.files[0])
-    output.style.backgroundImage = `url(${img})`
-    output.style.backgroundSize = "cover"
-};
+// var loadFile =function(event) {
+//     var output = document.querySelector('div#meme-image');
+//     var img = URL.createObjectURL(event.target.files[0])
+//     output.style.backgroundImage = `url(${img})`
+//     output.style.backgroundSize = "cover"
+// };
+
+var loadFile = function(imagemCarregada) {
+    var output = document.querySelector('#meme-image');
+    output.src = URL.createObjectURL(imagemCarregada.target.files[0]);
+}
 
 function typing() {
     var textImgInner = document.querySelector('input#text-input').value;
