4a7af6cbb58de3d2700f9062f2a452e7d10af9e3
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-02 19:10:15 -0300

    Desespero Total parte 2

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-let loadFile = function(event) {
-    let memeImage = document.getElementById("imagem");
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-    // memeImage.style.height = 300 + "px";
-    // memeImage.style.width = 300 + "px";
+let addImage = document.getElementById("meme-insert");
+let addText = document.getElementById("text-input");
+let memeText = document.getElementById("meme-text");
+
+function criandoUrl() {
+  let image = document.getElementById("meme-image");
+  image.src = URL.createObjectURL(addImage.files[0]);
+  image.style.height = 400 + "px";
+  image.style.width = 400 + "px";
 };
 
-function mostraTexto(){
-    let textoDigitado = document.getElementById("text-input").value;
-    let inserir=document.createElement("p");
-    inserir.innerText=textoDigitado;
+addImage.addEventListener("change", criandoUrl);
+let texto = document.getElementById("text-input");
+texto.addEventListener("input",exibir); 
 
-    let textoAnterior=document.getElementsByClassName("editImage")[0].getElementsByTagName("p")[0];
+function exibir(){
+document.getElementById("meme-text").innerHTML = texto.value;
+} 
 
-    if(textoAnterior){
-        document.getElementsByClassName("editImage")[0].removeChild(textoAnterior);
-    }
-    document.getElementsByClassName("editImage")[0].appendChild(inserir);
-}
+document.getElementById("button1").addEventListener("click", function () {
+  let imageContainer = document.getElementById("meme-image-container");
+  // console.log(imageContainer);
+  imageContainer.style.border = "3px dashed red";
+});
+document.getElementById("button2").addEventListener("click", function () {
+  let imageContainer2 = document.getElementById("meme-image-container");
+  imageContainer2.style.border = "5px dashed blue";
+  imageContainer2.style.borderStyle = "double"
+});
+document.getElementById("button3").addEventListener("click", function () {
+  let imageContainer3 = document.getElementById("meme-image-container");
+  imageContainer3.style.border = "6px groove green";
+});
