c945b47dbd9178cdf147d8848533f1aeb1468d61
renan.andreolle <renandreolle@hotmail.com>
2020-03-02 18:31:57 -0300

    Meu listener agora esta listenando

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 function previewFile() {
-    var preview = document.querySelector('img');
-    var file    = document.querySelector('input[type=file]').files[0];
-    var reader  = new FileReader();
-  
-    reader.onloadend = function () {
-      preview.src = reader.result;
-    }
-  
-    if (file) {
-      reader.readAsDataURL(file);
-    } else {
-      preview.src = "";
-    }
+  var preview = document.querySelector('img');
+  var file = document.querySelector('input[type=file]').files[0];
+  var reader = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
   }
-  let output = document.querySelector("#meme-text");
-  let meme = document.querySelector("#text-input");
-  output.addEventListener("keyup",function(){
-    
-        output.innerHTML = meme.target.value;
-  })
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "";
+  }
+}
+window.onload = () => {
+  document.getElementById("text-input").addEventListener("keyup",
+    event => document.getElementById("meme-text").innerHTML = event.target.value);
+}
