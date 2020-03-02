19a4965d20218cfa4a8bfc4bf185604d7c1ac83d
DiegoRafaelAF <draf0314@gmail.com>
2020-03-01 22:56:20 -0300

    Atualizando requisitos 2 tentativa

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 
-  let addImage = document.getElementById("meme-insert");
-  let addText = document.getElementById("text-input");
-  let memeText = document.getElementById("meme-text");
+let addImage = document.getElementById("meme-insert");
+let addText = document.getElementById("text-input");
+let memeText = document.getElementById("meme-text");
 
 
-  function criandoUrl() {
-    let image = document.getElementById("meme-image");
-    image.src = URL.createObjectURL(addImage.files[0]);
-    image.style.height = 400 + "px";
-    image.style.width = 400 + "px";
-  };
-  addImage.addEventListener("change", criandoUrl);
+function criandoUrl() {
+  let image = document.getElementById("meme-image");
+  image.src = URL.createObjectURL(addImage.files[0]);
+  image.style.height = 400 + "px";
+  image.style.width = 400 + "px";
+};
+addImage.addEventListener("change", criandoUrl);
 
-  function inputText() {
-    memeText.textContent = addText.value;
-  };
-  addText.addEventListener("beforeinput", inputText);
+function inputText() {
+  memeText.textContent = addText.value;
+};
+addText.addEventListener("beforeinput", inputText);
 
-document.getElementById("button1").addEventListener("click", function () {
-  let imageContainer = document.getElementById("meme-image-container");
-  imageContainer.style.border = "3px dashed red";
-});
+if (true) {
+  document.getElementById("button1").addEventListener("click", function () {
+    let imageContainer = document.getElementById("meme-image-container");
+    imageContainer.style.border = "3px dashed red";
+  });
 
-document.getElementById("button2").addEventListener("click", function () {
-  let imageContainer = document.getElementById("meme-image-container");
-  imageContainer.style.border = "5px dashed blue";
-  imageContainer.style.borderStyle = "double"
-});
+  document.getElementById("button2").addEventListener("click", function () {
+    let imageContainer = document.getElementById("meme-image-container");
+    imageContainer.style.border = "5px dashed blue";
+    imageContainer.style.borderStyle = "double"
+  });
 
-document.getElementById("button3").addEventListener("click", function () {
-  let imageContainer = document.getElementById("meme-image-container");
-  imageContainer.style.border = "6px groove green";
-});
+  document.getElementById("button3").addEventListener("click", function () {
+    let imageContainer = document.getElementById("meme-image-container");
+    imageContainer.style.border = "6px groove green";
+  });
+}
