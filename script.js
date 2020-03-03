45491282c3ff0aad3be3ac278c4d2e2ed88dd077
renan.andreolle <renandreolle@hotmail.com>
2020-03-02 23:33:25 -0300

    Ajustando os nomes para preencher requisitos

--- script.js
+++ script.js
@@ -17,20 +17,20 @@ window.onload = () => {
   document.getElementById("text-input").addEventListener("keyup",
     event => document.getElementById("meme-text").innerHTML = event.target.value);
   document.getElementById("meme-1").addEventListener("click", function() {
-    document.getElementById("meme-image").src = "./images/meme1.jpeg";})
+    document.getElementById("meme-image").src = "./imgs/meme1.jpeg";})
   document.getElementById("meme-2").addEventListener("click", function() {
-    document.getElementById("meme-image").src = "./images/meme2.jpeg";})
+    document.getElementById("meme-image").src = "./imgs/meme2.jpeg";})
   document.getElementById("meme-3").addEventListener("click", function() {
-    document.getElementById("meme-image").src = "./images/meme3.jpeg";})
+    document.getElementById("meme-image").src = "./imgs/meme3.jpeg";})
   document.getElementById("meme-4").addEventListener("click", function() {
-    document.getElementById("meme-image").src = "./images/meme4.jpeg";})
-  document.getElementById("button-1").addEventListener("click", function(){
+    document.getElementById("meme-image").src = "./imgs/meme4.jpeg";})
+  document.getElementById("button1").addEventListener("click", function(){
     document.getElementById("meme-image-container").classList.replace
     (document.getElementById("meme-image-container").classList[0],"button1");})
-  document.getElementById("button-2").addEventListener("click", function(){
+  document.getElementById("button2").addEventListener("click", function(){
     document.getElementById("meme-image-container").classList.replace
     (document.getElementById("meme-image-container").classList[0],"button2");})
-  document.getElementById("button-3").addEventListener("click", function(){
+  document.getElementById("button3").addEventListener("click", function(){
     document.getElementById("meme-image-container").classList.replace
     (document.getElementById("meme-image-container").classList[0],"button3");})
 }
