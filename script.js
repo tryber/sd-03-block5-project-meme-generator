1220bfd34d1e9c1491bd5314ddbd8e44faa8af92
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-03 12:57:52 -0300

    100%

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function loadFile() {
 inputImage.addEventListener("change", loadFile);
 
 function TextoDinamico() {
-  text.innerHTML = inputText.value
+  document.getElementById("meme-text").innerHTML = inputText.value
 }
-inputText.addEventListener("keydown", TextoDinamico);
+inputText.addEventListener("input", TextoDinamico);
 
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
