68cd2addca94750483ff55d4adec3c81ac94e42f
renan.andreolle <renandreolle@hotmail.com>
2020-03-02 23:16:43 -0300

    Acho que com isso vao-se os extras

--- script.js
+++ script.js
@@ -16,4 +16,21 @@ function previewFile() {
 window.onload = () => {
   document.getElementById("text-input").addEventListener("keyup",
     event => document.getElementById("meme-text").innerHTML = event.target.value);
+  document.getElementById("meme-1").addEventListener("click", function() {
+    document.getElementById("meme-image").src = "./images/meme1.jpeg";})
+  document.getElementById("meme-2").addEventListener("click", function() {
+    document.getElementById("meme-image").src = "./images/meme2.jpeg";})
+  document.getElementById("meme-3").addEventListener("click", function() {
+    document.getElementById("meme-image").src = "./images/meme3.jpeg";})
+  document.getElementById("meme-4").addEventListener("click", function() {
+    document.getElementById("meme-image").src = "./images/meme4.jpeg";})
+  document.getElementById("button-1").addEventListener("click", function(){
+    document.getElementById("meme-image-container").classList.replace
+    (document.getElementById("meme-image-container").classList[0],"button1");})
+  document.getElementById("button-2").addEventListener("click", function(){
+    document.getElementById("meme-image-container").classList.replace
+    (document.getElementById("meme-image-container").classList[0],"button2");})
+  document.getElementById("button-3").addEventListener("click", function(){
+    document.getElementById("meme-image-container").classList.replace
+    (document.getElementById("meme-image-container").classList[0],"button3");})
 }
