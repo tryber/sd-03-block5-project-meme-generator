15ab67aae59f5dffc4e0cec7f4929c4adcb2bd94
Hfreitas <hfreitas86@gmail.com>
2020-03-04 11:38:17 -0300

    index.html style.css script.js preliminares

---
+++ script.js
@@ -0,0 +1,34 @@
+// Exibe imagem: 
+  imageReader = function (event) {
+  let input = event.target;
+  let reader = new FileReader();
+  reader.onload = function() {
+    let imageURL = reader.result;
+    let showImg = document.getElementById('meme-image');
+    showImg.src = imageURL;
+  };
+  reader.readAsDataURL(input.files[0]);
+};
+
+// Exibe texto:
+const memeText = document.querySelector('input#text-input');
+
+function displayText() {
+  let textInput = memeText.value;
+  let textOutput = document.getElementById('meme-text');
+  textOutput.innerHTML = textInput;
+}
+
+//Event Listeners
+
+memeText.addEventListener('keyup', displayText);
+
+/*var textImg = document.querySelector('input#text-input');
+
+
+function typing() {
+  var textImgInner = document.querySelector('input#text-input').value;
+  document.querySelector('#meme-text').innerText = textImgInner;
+}
+
+textImg.addEventListener('keyup', typing);*/
