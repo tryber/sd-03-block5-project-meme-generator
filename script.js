5dd2ea0df39fb24ca799309ec33a7dd81ae4e1df
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-04 17:05:29 -0300

    adding imgs and meme buttons

--- script.js
+++ script.js
@@ -9,20 +9,45 @@ textBox.addEventListener("input", function() {
   textImage.innerHTML = textBox.value;
 });
 
-let button1 = document.querySelector('#button1');
-let button2 = document.querySelector('#button2');
-let button3 = document.querySelector('#button3');
-let divFix = document.querySelector('.div-fix');
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
+let divFix = document.querySelector(".div-fix");
 
-button1.addEventListener('click', function () {
-  divFix.style.border = '3px dashed red'
-})
+let meme1 = document.querySelector("#meme1");
+let meme2 = document.querySelector("#meme2");
+let meme3 = document.querySelector("#meme3");
+let meme4 = document.querySelector("#meme4");
 
-button2.addEventListener('click', function () {
-  divFix.style.border = '5px double blue'
-})
+button1.addEventListener("click", function() {
+  divFix.style.border = "3px dashed red";
+});
 
-button3.addEventListener('click', function () {
-  divFix.style.border = '6px groove green'
-})
+button2.addEventListener("click", function() {
+  divFix.style.border = "5px double blue";
+});
+
+button3.addEventListener("click", function() {
+  divFix.style.border = "6px groove green";
+});
+
+let memeImage = document.querySelector("#meme-image");
+meme1.addEventListener("click", function() {
+  memeImage.src = "./imgs/meme1.jpg";
+});
+
+meme2.addEventListener("click", function() {
+  memeImage.src = "./imgs/meme2.jpg";
+});
+
+meme3.addEventListener("click", function() {
+  memeImage.src = "./imgs/meme3.jpg";
+});
+
+meme4.addEventListener("click", function() {
+  memeImage.src = "./imgs/meme4.jpg";
+});
+
+let allMemes = document.querySelectorAll('.meme').length;
+let oneMeme = document.querySelector('.meme');
 
