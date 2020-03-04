e4fd935592fa4a875be8567d620ef4ebcf020a7b
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-04 16:21:48 -0300

    adding border buttons

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ textBox.addEventListener("input", function() {
   let textImage = document.getElementById("meme-text");
   textImage.innerHTML = textBox.value;
 });
+
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+let divFix = document.querySelector('.div-fix');
+
+button1.addEventListener('click', function () {
+  divFix.style.border = '3px dashed red'
+})
+
+button2.addEventListener('click', function () {
+  divFix.style.border = '5px double blue'
+})
+
+button3.addEventListener('click', function () {
+  divFix.style.border = '6px groove green'
+})
+
