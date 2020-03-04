9717c49fdc1e50130af2f5caf3b0ef9e27453df5
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-04 17:29:25 -0300

    adding img  files, changing css and refactoring js.

--- script.js
+++ script.js
@@ -9,45 +9,19 @@ textBox.addEventListener("input", function() {
   textImage.innerHTML = textBox.value;
 });
 
-let button1 = document.querySelector("#button1");
-let button2 = document.querySelector("#button2");
-let button3 = document.querySelector("#button3");
+let allButtons = document.querySelectorAll(".button");
 let divFix = document.querySelector(".div-fix");
-
-let meme1 = document.querySelector("#meme1");
-let meme2 = document.querySelector("#meme2");
-let meme3 = document.querySelector("#meme3");
-let meme4 = document.querySelector("#meme4");
-
-button1.addEventListener("click", function() {
-  divFix.style.border = "3px dashed red";
-});
-
-button2.addEventListener("click", function() {
-  divFix.style.border = "5px double blue";
-});
-
-button3.addEventListener("click", function() {
-  divFix.style.border = "6px groove green";
-});
+for (let i = 0; i < allButtons.length; i += 1) {
+  let divBorders = ["3px dashed red", "5px double blue", "6px groove green"];
+  allButtons[i].addEventListener("click", function() {
+    divFix.style.border = divBorders[i];
+  });
+}
 
 let memeImage = document.querySelector("#meme-image");
-meme1.addEventListener("click", function() {
-  memeImage.src = "./imgs/meme1.jpg";
-});
-
-meme2.addEventListener("click", function() {
-  memeImage.src = "./imgs/meme2.jpg";
-});
-
-meme3.addEventListener("click", function() {
-  memeImage.src = "./imgs/meme3.jpg";
-});
-
-meme4.addEventListener("click", function() {
-  memeImage.src = "./imgs/meme4.jpg";
-});
-
-let allMemes = document.querySelectorAll('.meme').length;
-let oneMeme = document.querySelector('.meme');
-
+let allMemes = document.querySelectorAll(".meme");
+for (let i = 0; i < allMemes.length; i += 1) {
+  allMemes[i].addEventListener("click", function() {
+    memeImage.src = `./imgs/meme${i + 1}.jpeg`;
+  });
+}
