74ca5240a843417a60b6f7280cacff872696d97e
Cezene <cezenesn@hotmail.com.br>
2020-03-04 16:32:48 -0300

    requisitos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function previewFile() {
+/* function previewFile() {
   var preview = document.querySelector('img');
   var file    = document.querySelector('input[type=file]').files[0];
   var reader  = new FileReader();
@@ -11,6 +11,17 @@ function previewFile() {
     reader.readAsDataURL(file);
   } else {
     preview.src = "";
-  }
-}
+  } */
+//}
 console.log("hello");
+
+let entradaTexto = document.getElementById("text-input");
+let entradaImagem = document.getElementById("meme-insert");
+let textoMeme = document.getElementById("meme-text");
+let meme = function(event){
+  let memeImg = document.getElementById("meme-image");
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
+}
+entradaTexto.addEventListener("keyup",function(){
+  textoMeme.innerHTML = this.value;
+})
