da897448278cf3f2587997393567657959010a8c
Gustavo <ggteodor07@gmai.com>
2020-03-03 21:03:34 -0300

    Ex 1 a 3 com a ajuda do Raphael

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let botaoImg = document.getElementById('add-text');
-botaoImg.addEventListener('click',function(){
-  document.querySelector('#meme-text').innerHTML = document.querySelector('#text-input').value;
+
+let entradaTexto = document.getElementById("text-input");
+let entradaImagem = document.getElementById("meme-insert");
+let textoMeme = document.getElementById("meme-text");
+
+let meme = function(event){
+  let memeImg = document.getElementById("meme-image");
+  memeImg.src = URL.createObjectURL(event.target.files[0]);
+}
+
+entradaTexto.addEventListener("keyup",function(){
+  textoMeme.innerHTML = this.value; 
 })
+
+
+
