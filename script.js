6903d0f95732e1a9586dd4dfcd618de52f1d1b91
leonardodamasceno8 <leonardodamasceno8@users>
2020-03-04 14:29:27 -0300

    genneteroor

---
+++ script.js
@@ -0,0 +1,22 @@
+function mostraImagem() {
+    var preview = document.querySelector('img');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+  
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
+}
+
+function setTexto(){
+    var meme = document.getElementById('text-input').value;
+
+    
+}
+    
