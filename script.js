93753d5ed8d10faa7db4250413e01f7df4b57308
RobertoAsgon <60623542+RobertoAsgon@users.noreply.github.com>
2020-03-05 00:53:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ function uploadIMG(event) {
     image.style.width = 400 + "px";
 };
 //Escolhendo Imagem
-function selectedIMG(memeImage) {
-    var image = document.getElementById('meme-image');
-    image.src = memeImage;    
-    image.style.height = 400 + "px";
-    image.style.width = 400 + "px";
-    if (keyBoard >= 1) {
-        image.style.height = 390 + "px";
-        image.style.width = 390 + "px";
-    } 
+//function selectedIMG(memeImage) {
+//    var image = document.getElementById('meme-image');
+//   image.src = memeImage;    
+//    image.style.height = 400 + "px";
+//    image.style.width = 400 + "px";
+//    if (keyBoard >= 1) {
+//        image.style.height = 390 + "px";
+//        image.style.width = 390 + "px";
+//    } 
+//}
+
+const img = document.getElementsByClassName('memeImage')
+let image = document.getElementById('meme-image');
+
+for (let i = 0; i < img.length; i++) {
+  img[i].addEventListener('click', function () {
+    image.src = img[i].src;
+  })
+
 }
 //Configurando o Texto
 let textoCima = document.getElementById("text-input")
