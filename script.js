c8586b2f3e7b7b7ea5788477823bd4aabea01b26
mirellaalves <mirella.alves@gmail.com>
2020-03-05 18:30:26 -0300

    Novas tentativas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-let textoInserido = document.getElementById("meme-image-container")
+let textoInserido = document.getElementById("")
 let textoDigitado = document.getElementById("text-input")
 textoInserido.innerHTML = textoDigitado.value
+
