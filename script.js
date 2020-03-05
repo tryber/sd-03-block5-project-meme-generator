9ef8694f119ab055e07c1696c27d3bae47c21463
wenderson belko <contato@wendersonbelko.com>
2020-03-05 20:40:19 -0300

    80% pfvr pq eu nao quero mais fazer projeto ;-;

---
+++ script.js
@@ -0,0 +1,12 @@
+document
+  .querySelector("#text-input")
+  .addEventListener("input", function(event) {
+    document.querySelector("#meme-text").innerHTML = document.querySelector(
+      "#text-input"
+    ).value;
+  });
+
+var loadFile = function(event) {
+  var image = document.getElementById("meme-image");
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
