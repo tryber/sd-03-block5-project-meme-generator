9fffecb8b28d9a8891e01c55547c8eaa5d1fa3ec
William Igor <williamigorsilva@gmail.com>
2020-03-04 22:29:25 -0300

    Alrerando css border

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let botao3 = document.getElementById("button3");
 botao3.addEventListener('click', borda3);
 
 
-let borda = document.getElementById("meme-image-container");
+let borda = document.getElementById("meme-image");
 
 function borda1() {
   borda.style.borderColor = "red";
