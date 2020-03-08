4df5521e8a5be64b5508c546e71a2d385028269f
mirellaalves <mirella.alves@gmail.com>
2020-03-08 17:43:06 -0300

    Alteracoes texto e imagem

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let textoInserido = document.getElementById("meme-image-container")
-let textoDigitado = document.getElementById("insereTexto")
-textoInserido.innerHTML = textoDigitado.value
+let fotoNaTela = document.getElementById("meme-image");
+let fotoEscolhida = document.getElementById("meme-insert");
+
+function memeImage() {
+    fotoNaTela.src =  URL.createObjectURL(fotoEscolhida.files[0]);
+}
+fotoEscolhida.addEventListener("input", memeImage);
+
+
+let textoNaFoto = document.getElementById("meme-text");
+let textoDigitado = document.getElementById("text-input");
+
+function memeText() {
+    textoNaFoto.innerHTML = textoDigitado.value;
+}
+textoDigitado.addEventListener("keyup", memeText);
