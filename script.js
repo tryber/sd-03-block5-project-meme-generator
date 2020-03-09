11e3a5bb6a1176ea5f8415e69eee0de5320cb3df
Cezene <cezenesn@hotmail.com.br>
2020-03-09 02:20:36 -0300

    comitando

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let entradaTexto = document.getElementById("text-input");
-let entradaImagem = document.getElementById("meme-insert");
-let textoMeme = document.getElementById("meme-text");
-let meme = function(event){
-  let memeImg = document.getElementById("meme-image");
-  memeImg.src = URL.createObjectURL(event.target.files[0]);
+function previewFile() {
+  var preview = document.querySelector('img');
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "";
+  }
 }
-entradaTexto.addEventListener("keyup",function(){
-  textoMeme.innerHTML = this.value;
-})
+function pr() {
+  document.getElementById("result").innerHTML = document.getElementById('text-input').value;
+}
