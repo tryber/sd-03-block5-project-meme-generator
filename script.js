cd40ae1b8a1cbcb08d738d9136aa743ce1591b1b
RafaelPBarbosa <rafael.coper@outlook.com>
2020-03-10 01:41:13 -0300

    Primeira imagem no quadro

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
+    let memeImage = document.getElementById("meme-image");
     let loadFile = function (event) {
-        let memeImage = document.getElementById("meme-image");
         memeImage.src = URL.createObjectURL(event.target.files[0]);       
     };
 
@@ -15,5 +15,26 @@ window.onload = () => {
     }
     
     txtInput.addEventListener('keyup', escrever)
+
+    /*const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+    const button3 = document.getElementById("button3");
+    let container = document.getElementById("meme-image-container");
+    
+    function trocarBorda1 (){
+        container.style.border = "3px dashed red"
+    }
+    button1.addEventListener('click', trocarBorda1);*/
+
+    const meme1 = document.getElementById("meme-1")
+    const meme2 = document.getElementById("meme-2")
+    const meme3 = document.getElementById("meme-3")
+    const meme4 = document.getElementById("meme-4")
+    
+    meme1.addEventListener('click', function(){
+        memeImage.src = meme1.src 
+    })
+
 }   
+
 
