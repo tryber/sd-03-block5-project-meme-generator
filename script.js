e841946e67b21ef1e7c98642d64611c4ecbb7e66
Ga Pimentel <cadgap@gmail.com>
2020-03-10 18:21:39 -0300

    Bônus 1 - bordas

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function inserirTexto() {
     let texto = document.getElementById("text-input").value;
     document.getElementById("meme-text").innerHTML = texto;
 }
+
 function visualizarImagem() {
     let memeImage = document.getElementById("meme-insert");
     let reader = new FileReader();
@@ -11,4 +12,16 @@ function visualizarImagem() {
         memeInsert.src = dataURL;
     }
     reader.readAsDataURL(memeImage.files[0]);
+}
+
+function bordaDashed() {
+  document.getElementById('meme-image-container').style.border = '3px dashed red'
+}
+
+function bordaDouble() {
+  document.getElementById('meme-image-container').style.border = '5px double blue'
+}
+
+function bordaGroove() {
+  document.getElementById('meme-image-container').style.border = '6px groove green'
 }
