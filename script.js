4210968898bf7db294585823307297e5c1f170cf
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-10 19:14:36 -0300

    corrigindo erros do climate

--- script.js
+++ script.js
@@ -10,11 +10,9 @@ function changeImage() {
     var preview = document.querySelector('img');
     var file    = document.querySelector('input[type=file]').files[0];
     var reader  = new FileReader();
-      
     reader.onloadend = function () {
         preview.src = reader.result;
     }
-      
     if (file) {
           reader.readAsDataURL(file);
     } else {
