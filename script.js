4e65f3971074a80b19982d1fc4fecc3df97b2ea1
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-10 19:35:14 -0300

    requisito bonus 1

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let texto = document.getElementById('text-input');
 let imagem = document.getElementById('meme-insert');
 let meme_image;
 let caixaDeTexto = document.getElementById('text-input');
+let bottons = document.querySelectorAll('button');
+let espacoDoMeme = document.getElementById('meme-image-container');
 function changeText() {
     var alteraTexto = document.getElementById('meme-text');
     alteraTexto.innerHTML = texto.value;
@@ -18,6 +20,19 @@ function changeImage() {
     } else {
           preview.src = '';
     }
+}
+for (let i = 0; i < 3; i += 1) {
+    bottons[i].addEventListener('click', function() {
+        if (i == 0) {
+            espacoDoMeme.style.border = '3px dashed red';
+        }
+        else if (i == 1) {
+            espacoDoMeme.style.border = '5px double blue';
+        }
+        else {
+            espacoDoMeme.style.border = '6px groove green';
+        }
+    });
 }
 caixaDeTexto.addEventListener('keyup', changeText);
 imagem.addEventListener('change', changeImage);
