1832794a3e86834c654f70eec4a0b89649e735fd
Gustavo <ggteodor07@gmai.com>
2020-03-10 23:28:41 -0300

    tryind button

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let entradaTexto = document.getElementById("text-input");
 let entradaImagem = document.getElementById("meme-insert");
 let textoMeme = document.getElementById("meme-text");
+let botao1 = document.getElementById('button1');
+let botao2 = document.getElementById('button2');
+let botao3 = document.getElementById('button3');
 
 let meme = function(event){
   let memeImg = document.getElementById("meme-image");
@@ -12,5 +15,24 @@ entradaTexto.addEventListener("keyup",function(){
   textoMeme.innerHTML = this.value; 
 })
 
+botao1.addEventListener('click',function(){
+  let container = document.getElementById('meme-image-container');
+  container.style.borderWidth = '3px';
+  container.style.borderColor = 'red';
+  container.style.borderStyle = 'dashed';
+})
 
+botao2.addEventListener('click',function(){
+  let container = document.getElementById('meme-image-container');
+  container.style.borderWidth = '5px';
+  container.style.borderColor = 'blue';
+  container.style.borderStyle = 'double';
+})
+
+botao3.addEventListener('click',function(){
+  let container = document.getElementById('meme-image-container');
+  container.style.borderWidth = '6px';
+  container.style.borderColor = 'green';
+  container.style.borderStyle = 'groove';
+})
 
