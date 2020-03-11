8d912f1093bcf1d5e277e031c49bbdf55c237cf8
Gustavo <ggteodor07@gmai.com>
2020-03-11 00:58:54 -0300

    1 try 100%

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ let textoMeme = document.getElementById("meme-text");
 let botao1 = document.getElementById('button1');
 let botao2 = document.getElementById('button2');
 let botao3 = document.getElementById('button3');
+let meme1 = document.getElementById('meme-1');
+let meme2 = document.getElementById('meme-2');
+let meme3 = document.getElementById('meme-3');
+let meme4 = document.getElementById('meme-4');
+let memeImg = document.getElementById("meme-image");
+
 
 let meme = function(event){
-  let memeImg = document.getElementById("meme-image");
   memeImg.src = URL.createObjectURL(event.target.files[0]);
 }
 
@@ -34,5 +39,21 @@ botao3.addEventListener('click',function(){
   container.style.borderWidth = '6px';
   container.style.borderColor = 'green';
   container.style.borderStyle = 'groove';
+})
+
+meme1.addEventListener('click',function(){
+  memeImg.src = meme1.src;
+})
+
+meme2.addEventListener('click',function(){
+  memeImg.src = meme2.src;
+})
+
+meme3.addEventListener('click',function(){
+  memeImg.src = meme3.src;
+})
+
+meme4.addEventListener('click',function(){
+  memeImg.src = meme4.src;
 })
 
